45e4cb2232cb67ac2bbe694a40ff6f254ad978f3
=vallabhdpn <=vallabhdp@tuta.io>
2024-07-27 12:25:25 +0530

    Initial commit

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,11 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+<<<<<<< HEAD
 module BookRadarNew
+=======
+module BookRadar
+>>>>>>> b0d0202 (Initial commit)
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
